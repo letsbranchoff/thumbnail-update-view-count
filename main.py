a7a3c9f9d3915ef12b3509167d7300f68900943c
Let's Branch Off <dom@letsbranchoff.com>
2024-07-07 21:45:08 +0100

    reading in svg file

--- main.py
+++ main.py
@@ -1,5 +1,22 @@
+import os
+from xml.dom import minidom
+
+
+def open_svg(
+    path: str = os.path.join("assets", "update-image-test-python.svg")
+) -> minidom.Document | None:
+    try:
+        return minidom.parse(path)
+    except:
+        return None
+
+
 def main():
-    print("update thumbnail...")
+    svg = open_svg()
+
+    if not svg:
+        print("Failed to load SVG image...")
+        return
 
 
 if __name__ == "__main__":
