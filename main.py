b502642a3fe300dd0b0685facb1c71e55c39ece5
Let's Branch Off <dom@letsbranchoff.com>
2024-07-07 21:57:09 +0100

    update svg value and save svg

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+from pathlib import Path
 from xml.dom import minidom
 
 
@@ -11,12 +12,28 @@ def open_svg(
         return None
 
 
+def update_text_in_svg(svg: minidom.Document, value: str) -> None:
+    svg.getElementsByTagName("tspan")[0].firstChild.nodeValue = value
+
+
+def save_svg(svg: minidom.Document, path: str = os.path.join("output")):
+    Path(path).mkdir(exist_ok=True)
+    with open(os.path.join(path, "updated.svg"), "w") as f:
+        f.write(svg.toxml())
+
+
 def main():
     svg = open_svg()
 
     if not svg:
         print("Failed to load SVG image...")
         return
+
+    update_text_in_svg(svg=svg, value="Test")
+
+    save_svg(svg=svg)
+
+    print("Updated SVG.")
 
 
 if __name__ == "__main__":
