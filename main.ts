1f37534fb45e08a385c3f0e1150a2586a1294d32
Let's Branch Off <dom@letsbranchoff.com>
2024-07-08 13:15:11 +0100

    read svg to str - debugging in vscode

--- main.ts
+++ main.ts
@@ -1,5 +1,22 @@
+import fs from "node:fs";
+
+function openSvg(path = "./assets/thumbnail-template.svg"): string {
+  try {
+    return fs.readFileSync((path = path), "utf8");
+  } catch (err) {
+    console.error(err);
+    process.exitCode = 1;
+  }
+}
+
 function main() {
-  console.log("hello from main");
+  const svg = openSvg();
+
+  if (process.exitCode == 1) {
+    return;
+  }
+
+  console.log(svg);
 }
 
 if (typeof require !== "undefined" && require.main === module) {
